1e301f5e2275308d76381d67b27f780e1b5444d0
Einstein <einsteinnnamah@gmail.com>
2022-08-14 02:23:50 +0100

    values and Variables

--- Fundamentals_one/Readme.md
+++ Fundamentals_one/Readme.md
@@ -1,3 +1,3 @@
 This Folder includes the fundamentals of javaScript Topics 
-1. Basic Operations
+1. Values and variables
 2. 
---
+++ Fundamentals_one/index.js
@@ -0,0 +1,24 @@
+/////////////////////1. Values and Variable Types//////////////////////////////
+
+//Value is basically is the smallest unit of information in javaScript e.g//
+console.log("Hello World"); // "hello world" is the value
+console.log(1); // 1 is the value
+
+//Variable is basically a container for a value, we can store a value in a variable e.g//
+let Firstname = "John"; // so here we basically assign the value "John" to the variable name
+//for us to access the value of the variable we need to use the variable name e.g//
+console.log(Firstname); // we can then access the value of the variable name
+//Checking your console we indeed get John printed out//
+
+//Nb:We can also use a variable name more than once e.g//
+//Rules for naming variables: 
+//1. Variable names can only contain letters, numbers, and underscores
+//2. Variable names are case sensitive (upper and lower case letters are different) e.g let firstName = "John" and let firstName = "john" are two different variables
+//3. Variable names should not be JS keywords
+//4. Variable names should not start with a number
+//5. Variable names should not be reserved keywords
+//6. Variable names should not be too long or short
+//7. Variable names must be descriptive e.g "let = myFirstJob = 'Programmer' not let job1 = 'Programmer'"
+
+
+
